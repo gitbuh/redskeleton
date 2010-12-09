3a45aecbea05b25a2a55b3093d89c3a3a98ced0f
Nick <no@spam.com>
2010-12-09 09:47:50 -0500

    m

--- app/view/widget/login.php
+++ app/view/widget/login.php
@@ -6,7 +6,7 @@ class App_View_Widget_Login extends App_View_Widget {
       User logs in
   */
   public static function login () {
-    Model_User::login($_POST['l'], $_POST['p']);
+    Model_User::login($_REQUEST['l'], $_REQUEST['p']);
   }
   /**
       User logs in
